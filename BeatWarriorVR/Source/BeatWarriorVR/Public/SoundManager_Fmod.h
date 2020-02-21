096c3deb5b8a5c61ac32a7433bd479ba0becb77a
Jostyck9 <hugo.berthome@epitech.eu>
2020-02-21 18:28:45 +0100

    adding new weapon and adding modes

--- BeatWarriorVR/Source/BeatWarriorVR/Public/SoundManager_Fmod.h
+++ BeatWarriorVR/Source/BeatWarriorVR/Public/SoundManager_Fmod.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <deque>
 #include "CoreMinimal.h"
 
 namespace FMOD
@@ -10,6 +12,7 @@ namespace FMOD
 	class System;
 	class Channel;
 	class Sound;
+	class DSP;
 };
 
 /**
@@ -28,8 +31,35 @@ public:
 	void pauseSound(bool unPause = false);
 	bool isPlaying();
 
+	void update();
+	int initializeSpectrum_Linear(int maxBands);
+	int initializeSpectrum_Log(int bandsPerOctave);
+	void initializeBeatDetector();
+	void getSpectrum_Linear(float* spectrum, float* averageSpectrum);
+	void getSpectrum_Log(float* spectrum, float* averageSpectrum);
+	void getBeat(float* spectrum, float* averageSpectrum, bool& isBass, bool& isLowM);
+
 private:
 	FMOD::System* _system;
 	FMOD::Channel* _channel;
 	FMOD::Sound* _sound;
+
+	FMOD::DSP* _dsp;
+	int _windowSize;
+	float _samplingFrequency;
+
+	std::vector<int> _numSamplesPerBar_linear;
+	std::vector<int> _numSamplesPerBar_log;
+
+	typedef std::deque<std::vector<float> > FFTHistoryContainer;
+
+	int _FFThistory_MaxSize;
+	std::vector<int> _beatDetector_bandLimits;
+	FFTHistoryContainer _FFTHistory_linear;
+	FFTHistoryContainer _FFTHistory_log;
+	FFTHistoryContainer _FFTHistory_beatDetector;
+
+	static void fillAverageSpectrum(float* averageSpectrum, int numBands, const FFTHistoryContainer& fftHistory);
+	static void fillVarianceSpectrum(float* varianceSpectrum, int numBands, const FFTHistoryContainer& fftHistory, const float* averageSpectrum);
+	static float beatThreshold(float variance);
 };
